b778262a4e4e8f3bb8ebe7809a161c34bcd03b84
Kroos <kroosvi@gmail.com>
2023-01-31 20:29:46 -0500

    refactor impl use intead of a funciton

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@ use std::{env, fs};
 fn main() {
     let args : Vec<String> = env::args().collect();
 
-    //let (query , file_path) = parse_config(&args);
-    let config = parse_config(&args);
+    //V1: let (query , file_path) = parse_config(&args);
+    //V2: let config = parse_config(&args);
+    let config = Config::new(&args);
 
     // println!("We are reading {query}");
     println!("We are reading {}", config.query);
     // println!("From this file path {file_path}");
     println!("File path {}", config.file_path);
-
-
 
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
@@ -23,7 +22,16 @@ struct Config{
     file_path :String,
 }
 
-fn parse_config(args : &[String]) -> Config{  //(&str , &str)
+impl Config{
+    fn new(args : &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {query: query.to_string(),file_path : file_path.to_string()}
+    }
+}
+
+/* fn parse_config(args : &[String]) -> Config{  //(&str , &str)
     // let query = &args[1];
     let query = &args[1].clone(); //hard copy
     // let file_path = &args[2];
@@ -31,4 +39,4 @@ fn parse_config(args : &[String]) -> Config{  //(&str , &str)
 
     // (query, file_path)
     Config {query: query.to_string(),file_path : file_path.to_string()}
-}
+} */
