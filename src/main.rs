f5080cace77c915f9e74246f55c98e62d0b3e40f
Kroos Xiang <kroosvi@gmail.com>
2023-02-06 11:16:12 -0500

    udpate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::{env, fs};
+use std::{env, fs,process};
+use std::result::Result;
+use std::error::Error;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
 
     //V1: let (query , file_path) = parse_config(&args);
     //V2: let config = parse_config(&args);
-    let config = Config::new(&args).unwarp_or_else(
+    let config = Config::new(&args).unwrap_or_else(
         |err|{
-            println!("Problem parsing argument: {err}"):
+            println!("Problem parsing argument: {err}");
             process::exit(1);
         }
     );
@@ -17,9 +19,9 @@ fn main() {
     // println!("From this file path {file_path}");
     println!("File path {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    // let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    // println!("With text:\n{contents}");
+    run(config); 
 }
 
 struct Config{
@@ -27,10 +29,11 @@ struct Config{
     file_path :String,
 }
 
+
 impl Config{
-    fn new(args : &[String]) -> Result<config , &'static str>{
+    fn new(args : &[String]) -> Result<Config , &'static str>{
         if args.len() < 3{ 
-            panic!("Not Enough arguments");
+            return Err("Not Enough arguments");
         }
 
         let query = args[1].clone();
@@ -40,6 +43,13 @@ impl Config{
     }
 }
 
+fn run(config : Config) -> Result<() , Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text : {contents}");
+
+    Ok(())
+}
 /* fn parse_config(args : &[String]) -> Config{  //(&str , &str)
     // let query = &args[1];
     let query = &args[1].clone(); //hard copy
